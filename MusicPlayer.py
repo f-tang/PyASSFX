adfae8555ee5f332d489933d73b695461e212ab4
f-tang <miketfh@sjtu.edu.cn>
2023-02-26 21:23:28 -0500

    Added fade in/fade out to the last and the first sub-line.

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -7,6 +7,7 @@ PREV_LINE_NUM = 3
 NEXT_LINE_NUM = 3
 MOVEMENT_TIME_MS = 300
 LINE_DISTANCE_RATE = 1.
+FADE_TIME = 300
 
 
 def make_cur_line(line: pyonfx.Line):
@@ -34,23 +35,26 @@ def make_prev_line(current_line: pyonfx.Line,
     new_line.style = "SubStyle"
     new_line.actor = prev_line.actor
     new_line.y = current_line.y - line_dist * idx
+    movement_time = min(MOVEMENT_TIME_MS, new_line.duration)
+    fade_time = min(FADE_TIME, new_line.duration)
     if 0 < idx < PREV_LINE_NUM:
-        new_line.text = (
-            "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)}%s"
-        % (
-                current_line.center, current_line.middle - line_dist * (idx - 1),
-                current_line.center, current_line.middle - line_dist * idx,
-                0, MOVEMENT_TIME_MS,
-                prev_line.text
-            )
-        )
-    elif idx == PREV_LINE_NUM:
         new_line.text = (
                 "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)}%s"
                 % (
                     current_line.center, current_line.middle - line_dist * (idx - 1),
                     current_line.center, current_line.middle - line_dist * idx,
-                    0, MOVEMENT_TIME_MS,
+                    0, movement_time,
+                    prev_line.text
+                )
+        )
+    elif idx == PREV_LINE_NUM:
+        new_line.text = (
+                "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\fad(%d,%d)}%s"
+                % (
+                    current_line.center, current_line.middle - line_dist * (idx - 1),
+                    current_line.center, current_line.middle - line_dist * idx,
+                    0, movement_time,
+                    0, fade_time,
                     prev_line.text
                 )
         )
@@ -67,23 +71,26 @@ def make_next_line(current_line: pyonfx.Line,
     new_line.style = "SubStyle"
     new_line.actor = next_line.actor
     new_line.y = current_line.y + line_dist * idx
+    movement_time = min(MOVEMENT_TIME_MS, new_line.duration)
+    fade_time = min(FADE_TIME, new_line.duration)
     if 0 < idx < NEXT_LINE_NUM:
         new_line.text = (
                 "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)}%s"
                 % (
                     current_line.center, current_line.middle + line_dist * (idx + 1),
                     current_line.center, current_line.middle + line_dist * idx,
-                    0, MOVEMENT_TIME_MS,
+                    0, movement_time,
                     next_line.text
                 )
         )
     elif idx == NEXT_LINE_NUM:
         new_line.text = (
-                "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)}%s"
+                "{\\an5\\fscx75\\fscy75\\move(%.3f,%.3f,%.3f,%.3f,%d,%d)\\fad(%d,%d)}%s"
                 % (
                     current_line.center, current_line.middle + line_dist * (idx + 1),
                     current_line.center, current_line.middle + line_dist * idx,
-                    0, MOVEMENT_TIME_MS,
+                    0, movement_time,
+                    fade_time, 0,
                     next_line.text
                 )
         )
